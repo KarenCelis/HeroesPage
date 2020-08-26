10d3b2d46d5c105a5cf05ca802459b53cf6bd031
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-08-26 11:25:19 -0500

    custom filter - title

--- heroes/src/app/pipes/title.pipe.ts
+++ heroes/src/app/pipes/title.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitlePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+
+    let header = 'Album '+value;
+    return header;
   }
 
 }

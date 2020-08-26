18c0eae8981aaa2c13afec09b37810455ab983c5
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-08-26 00:21:24 -0500

    Update app-routing.module.ts

--- heroes/src/app/app-routing.module.ts
+++ heroes/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'heroes', component: HeroesComponent},
+  {path : 'details', component: DetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
